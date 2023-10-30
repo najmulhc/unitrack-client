624e9db06c231a62345cbf1c5f7890c69a6d61e4
Najmul Huda Chowdhury Chayan <najmulhc@outlook.com>
2023-10-30 19:12:24 +0600

    login page done

--- src/pages/auth/Login.tsx
+++ src/pages/auth/Login.tsx
@@ -1,10 +1,11 @@
 import { useForm } from "react-hook-form";
-import PageContainer from "../../components/PageContainer";
-
+import {useState} from "react"
+import { Link } from "react-router-dom";
  
 
 const Login = () => {
   const { register, handleSubmit, reset } = useForm();
+  const [error, setError] = useState<string>("")
 
   const onSubmit = (data: any) => {
     console.log(data);
@@ -12,23 +13,49 @@ const Login = () => {
   };
 
   return (
-    <PageContainer>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <input
-          type="email" 
-          id="email-input"
-          placeholder="Enter email"
-          {...register("email")}
-        />
-        <input
-          type="password" 
-          id="password-input"
-          placeholder="Enter Password"
-          {...register("password")}
-        />
-        <button type="submit">Log In</button>
-      </form>
-    </PageContainer>
+    <main className="min-h-screen flex justify-center items-center ">
+      <div className="card w-[300px] bg-neutral p-4 gap-4 text-center ">
+        <h2 className="text-2xl">Login</h2>
+        <form className="flex flex-col gap-4" onSubmit={handleSubmit(onSubmit)}>
+          <div className="form-control">
+            <label className="label" htmlFor="email">
+              Enter email address
+            </label>
+            <input
+              className="input bg-base w-full "
+              type="email"
+              id="email-input"
+              placeholder="john@placeholder.com"
+              {...register("email")}
+            />
+          </div>
+          <div className="form-control">
+            <label className="label" htmlFor="password">
+              Enter password
+            </label>
+            <input
+              className="input w-full  input-bordered"
+              type="password"
+              id="password"
+              placeholder="*******"
+              {...register("password")}
+            />
+          </div>
+          
+          <button className="btn btn-primary w-full" type="submit">
+            Log in
+          </button>
+
+          <p>
+          New here?{" "}
+            <Link className="underline text-primary" to="/register">
+              {" "}
+              Register now
+            </Link>
+          </p>
+        </form>
+      </div>
+    </main>
   );
 };
 
