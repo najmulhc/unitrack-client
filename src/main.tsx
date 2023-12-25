a7bed97d1117863898bb858b6f11fcba2c12cc84
Najmul Huda Chowdhury Chayan <najmulhc@outlook.com>
2023-12-25 18:36:50 +0600

    will you fix now?

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import ReactDOM from "react-dom/client";
 import React from 'react'
 import "./index.css";
-import App from "../src/app.tsx";
+// import App from "./app.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <br />
   </React.StrictMode>
 );
