dfb905c0407ee9fdd8ec84091c139a85a98bd5df
Najmul Huda Chowdhury Chayan <najmulhc@outlook.com>
2023-12-26 19:50:12 +0600

    fix(app): removed environment variable files

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,7 +4,7 @@ const Home = () => {
   return (
     <PageContainer>
       <div className="prose">
-        <h1>Lorem ipsum dolor sit amet!</h1>
+        <h1>Lorem ipsum dolor sit amet!n</h1>
       </div>
     </PageContainer>
   );
