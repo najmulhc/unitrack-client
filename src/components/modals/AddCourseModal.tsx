bf380d1c90482e53c77c1c9e930d8b6c410f6825
Najmul Huda Chowdhury Chayan <najmulhc@outlook.com>
2023-12-31 22:33:04 +0600

    feat(app): Created a simple form for creating a course

--- src/components/modals/AddCourseModal.tsx
+++ src/components/modals/AddCourseModal.tsx
@@ -1,20 +1,120 @@
+import { useForm } from "react-hook-form";
 import { ModalProps } from "../../types";
 
- 
+interface FormData {
+  courseName: string;
+  courseCode: number;
+  session: number | string;
+}
+
 const AddCourseModal: React.FC<ModalProps> = ({ modalRef }) => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>();
+
+  const handleForm = (data: FormData) => {
+    console.log(data);
+  };
+
   return (
     <dialog className="modal modal-top sm:modal-middle" ref={modalRef}>
       <div className="modal-box prose">
         <form method="dialog">
-          {/* if there is a button in form, it will close the modal */}
-          <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
+          <button
+            type="submit"
+            className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2"
+          >
             ✕
           </button>
         </form>
         <h1>Please write something for the modal.</h1>
-        <form method="dialog" className="modal-backdrop">
-          <button type="submit" className="btn btn-neutral">
-            Test
+        <form
+          method="dialog"
+          onSubmit={handleSubmit(handleForm)}
+          className="modal-backdrop"
+        >
+          <label className="form-control w-full ">
+            <div className="label">
+              <span className="label-text">CourseName</span>
+            </div>
+            <input
+              type="text"
+              placeholder="Type here"
+              className={`input ${
+                errors.courseName ? "input-error" : "input-primary"
+              }  w-full text-white`}
+              {...register("courseName", {
+                required: {
+                  message: "Please write course name!",
+                  value: true,
+                },
+              })}
+            />
+            <div className="label">
+              <span className="label-text-alt text-error">
+                {errors.courseName ? errors?.courseName?.message : ""}
+              </span>
+            </div>
+          </label>
+          <label className="form-control w-full ">
+            <div className="label">
+              <span className="label-text">Course Code</span>
+            </div>
+            <input
+              type="number"
+              placeholder="Type here"
+              className={`input ${
+                errors.courseCode ? "input-error" : "input-primary"
+              }  w-full text-white`}
+              {...register("courseCode", {
+                required: {
+                  message: "Please write course code!",
+                  value: true,
+                },
+                min: {
+                  value: 101,
+                  message: "Invalid course code",
+                },
+                max: {
+                  value: 110,
+                  message: "Invalid course Code",
+                },
+              })}
+            />
+            <div className="label">
+              <span className="label-text-alt text-error">
+                {errors.courseCode ? errors?.courseCode?.message : ""}{" "}
+              </span>
+            </div>
+          </label>
+          <div className="w-full mb-4">
+            <label htmlFor="batch" className="label">
+              <span className="label-text">Batch</span>
+            </label>
+            <select
+              className={`input ${
+                errors.session ? "input-error" : "input-primary"
+              }  w-full text-white`}
+              {...register("session", {
+                required: "Please give the session.",
+              })}
+            >
+              <option value="">Select an option</option>
+              <option value="2020">2020</option>
+              <option value="2019">2019</option>
+            </select>
+            {errors.session && (
+              <label className="label">
+                <span className="label-text-alt text-error">
+                  {errors?.session?.message as string}
+                </span>{" "}
+              </label>
+            )}
+          </div>
+          <button type="submit" className="btn btn-primary">
+            Submit
           </button>
         </form>
       </div>
